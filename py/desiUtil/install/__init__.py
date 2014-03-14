8aeeed5106917012c1479ab7ea9020b2c6e3eb4c
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2014-03-14 13:56:18 +0000

    Add skeleton of desiInstall

---
+++ py/desiUtil/install/__init__.py
@@ -0,0 +1,10 @@
+# License information goes here
+# -*- coding: utf-8 -*-
+"""
+================
+desiUtil.install
+================
+
+This package contains code for installing DESI software products.
+"""
+
