98938fd6ce1415a08129efd7db3941a617188e46
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2014-04-15 14:30:53 +0000

    Adding functionality

--- py/desiUtil/install/main.py
+++ py/desiUtil/install/main.py
@@ -16,7 +16,8 @@ def main():
     main : int
         Exit status that will be passed to ``sys.exit()``.
     """
-    from sys import argv, path
+    from sys import argv, executable, path
+    from shutil import rmtree
     from os import environ, getenv, makedirs
     from os.path import basename, exists, isdir, join
     from argparse import ArgumentParser
@@ -26,8 +27,8 @@ def main():
     #
     # Parse arguments
     #
-    executable = basename(argv[0])
-    parser = ArgumentParser(description=__doc__,prog=executable)
+    xct = basename(argv[0])
+    parser = ArgumentParser(description=__doc__,prog=xct)
     parser.add_argument('-b', '--bootstrap', action='store_true', dest='bootstrap',
         help="Run in bootstrap mode to install the desiUtil product.")
     parser.add_argument('-d', '--default', action='store_true', dest='default',
@@ -39,6 +40,9 @@ def main():
         default=getenv('MODULESHOME'))
     parser.add_argument('-M', '--module-dir', action='store', dest='moduledir',
         metavar='DIR',help="Install module files in DIR.",default='')
+    parser.add_argument('-r', '--root', action='store', dest='root',
+        metavar='DIR', help='Set or override the value of $DESI_PRODUCT_ROOT',
+        default=getenv('DESI_PRODUCT_ROOT'))
     parser.add_argument('-t', '--test', action='store_true', dest='test',
         help='Test mode.  Do not actually install anything.')
     parser.add_argument('-u', '--url', action='store',dest='url',
@@ -55,6 +59,7 @@ def main():
     parser.add_argument('product_version',nargs='?',default='NO VERSION',
         help='Version of product to install.')
     options = parser.parse_args()
+    debug = options.test or options.verbose
     #
     # Print version if requested.
     #
@@ -128,6 +133,22 @@ def main():
     #if isdir(working_dir):
     #    os.environ[product_dir] = './'+working_dir
     #
+    # Pick an install directory
+    #
+    nersc = None
+    try:
+        nersc = environ['NERSC_HOST']
+    except KeyError:
+        pass
+    if options.root is None or not isdir(options.root):
+        if nersc is not None:
+            options.root = join('/project/projectdirs/desi/software',nersc)
+        else:
+            print("DESI_PRODUCT_ROOT is missing or not set.")
+            return 1
+    install_dir = join(options.root,baseproduct,baseversion)
+    makedirs(install_dir)
+    #
     # Prepare to configure module.
     #
     module_keywords = dict()
@@ -156,15 +177,42 @@ def main():
     #
     module_keywords['pyversion'] = "python{0:d}.{1:d}".format(*sys.version_info)
     #
+    # Run the install
+    #
+    command = [executable, 'setup.py', 'install', '--prefix={0}'.format(install_dir)]
+    if options.verbose:
+        print(' '.join(command))
+    proc = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    if options.verbose:
+        print(out)
+    if len(err) > 0:
+        print("Error during installation:")
+        print(err)
+        return 1
+    #
     # Process the module file.
     #
     module_file = join(working_dir,'etc',baseproduct+'.module')
     if exists(module_file):
+        if not isdir(join(options.moduledir,baseproduct)):
+            makedirs(join(options.moduledir,baseproduct))
+        install_module_file = join(options.moduledir,baseproduct,baseversion)
         with open(module_file) as m:
             mod = m.read().format(**module_keywords)
-        new_module_file = os.path.join(working_dir,'etc',baseproduct+'_'+baseversion+'.module')
-        with open(new_module_file,'w') as m:
+        with open(install_module_file,'w') as m:
             m.write(mod)
+        if options.default:
+            dot_version = '''#%Module1.0
+set ModulesVersion "{0}"
+'''.format(baseversion)
+            install_version_file = join(options.moduledir,baseproduct,'.version')
+            with open(install_version_file,'w') as v:
+                v.write(dot_version)
+    #
+    # Clean up
+    #
+    rmtree(working_dir)
     return 0
 #
 #
