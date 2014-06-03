064cab8f8dfffa38876349fb22d996f23471b4ca
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2014-06-03 20:01:23 +0000

    Add version.py

--- py/desiUtil/__init__.py
+++ py/desiUtil/__init__.py
@@ -1,7 +1,5 @@
 # License information goes here
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
-# The line above will help with 2to3 support.
 """
 ========
 desiUtil
@@ -11,11 +9,14 @@ This package provides low-level utilities for general use by DESI_.
 
 .. _DESI: http://desi.lbl.gov
 """
+#
+from __future__ import absolute_import, division, print_function, unicode_literals
+# The line above will help with 2to3 support.
 from .install import version
 #
 # Set version string.
 #
-__version__ = version($HeadURL$)
+__version__ = version('$HeadURL$')
 #
 # Clean up namespace
 #
---
+++ py/desiUtil/install/version.py
@@ -0,0 +1,38 @@
+# License information goes here
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+# The line above will help with 2to3 support.
+def version(headurl):
+    """Returns the version of a package.
+
+    Parameters
+    ----------
+    headurl : str
+        A svn HeadURL string.
+
+    Returns
+    -------
+    version : str
+        A PEP 386-compatible version string.
+
+    Notes
+    -----
+    The version string should be compatible with `PEP 386`_ and
+    `PEP 440`_.
+
+    .. _`PEP 386`: http://legacy.python.org/dev/peps/pep-0386/
+    .. _`PEP 440`: http://legacy.python.org/dev/peps/pep-0440/
+    """
+    from . import get_svn_devstr, most_recent_tag
+    if headurl.find('tags') > 0:
+        # myproduct = headurl[0:headurl.find('tags')-1].split('/')[-1]
+        myversion = headurl[headurl.find('tags')+5:].split('/')[0]
+    elif (headurl.find('trunk') > 0) or (headurl.find('branches') > 0):
+        url = headurl[10:len(headurl)-2]
+        findstr = ('branches','trunk')[int(headurl.find('trunk') > 0)]
+        myproduct = headurl[0:headurl.find(findstr)-1].split('/')[-1]
+        tagurl = url[0:url.find(findstr)]+'tags'
+        myversion = most_recent_tag(tagurl) + '.dev' + get_svn_devstr(myproduct)
+    else:
+        myversion = '0.0.1.dev'
+    return myversion
