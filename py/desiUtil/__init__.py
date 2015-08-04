df6d44efe9ea1adb913030ff87d02ff4c3a0cd19
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-08-04 17:23:56 -0400

    fix version string

--- py/desiUtil/__init__.py
+++ py/desiUtil/__init__.py
@@ -12,12 +12,7 @@ This package provides low-level utilities for general use by DESI_.
 #
 from __future__ import absolute_import, division, print_function, unicode_literals
 # The line above will help with 2to3 support.
-from .install import version
 #
 # Set version string.
 #
-__version__ = version('$HeadURL$')
-#
-# Clean up namespace
-#
-del version
+__version__ = '0.6.0.dev'
