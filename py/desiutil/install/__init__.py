dd5ec09e61e521d0759f9cc18bcf4de2b51aeef4
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-10-20 16:16:21 -0400

    remove duplicate modules

--- py/desiutil/install/__init__.py
+++ py/desiutil/install/__init__.py
@@ -11,12 +11,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 # The line above will help with 2to3 support.
 from .desi_install import DesiInstall
 from .dependencies import dependencies
-from .generate_doc import generate_doc
-from .get_product_version import get_product_version
 from .get_svn_devstr import get_svn_devstr
 from .git_version import git_version
 from .known_products import known_products
 from .most_recent_git_tag import most_recent_git_tag
 from .most_recent_svn_tag import most_recent_svn_tag
-from .set_build_type import set_build_type
 from .svn_version import svn_version
