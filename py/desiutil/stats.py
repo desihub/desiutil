a0b374beecce8e24d4d2bd863983a3b00779324a
J. Xavier Prochaska <xavier@ucolick.org>
2016-01-28 13:03:48 -0800

    stats (percentile)

---
+++ py/desiutil/stats.py
@@ -0,0 +1,65 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+==================
+desiutils.funcfits
+==================
+
+Module for fitting simple functions to 1D arrays
+
+J. Xavier Prochaska, UC Santa Cruz
+Fall 2015
+
+.. _desispec: http://desispec.readthedocs.org
+"""
+from __future__ import print_function, absolute_import, division, unicode_literals
+
+import numpy as np
+from scipy.interpolate import interp1d
+#import pdb
+
+
+def perc(x, per=0.68):
+    """ Calculate the percentile bounds of a distribution,
+    i.e. for per=0.68, the code returns the upper and lower bounds
+    that encompass 68percent of the distribution.
+
+    Parameters
+    ----------
+      x : float
+        numpy array of values
+      per : float, optional
+          Percentile for the calculation
+
+    Returns
+    -------
+      xper : array
+        Value at lower, value at upper
+    """
+    #
+    npt = len(x)
+
+    # Sort
+    xsort = np.sort(x)
+    perx = (np.arange(npt)+1) / npt
+
+    f = interp1d(perx, xsort)
+
+    frac = (1.-per) / 2.
+
+    # Fill
+    xper = np.zeros(2)
+    try:
+        xper[0] = f( frac )
+    except ValueError:
+        xper[0] = np.min(x)
+
+    try:
+        xper[1] = f( 1.-frac )
+    except ValueError:
+        xper[1] = np.max(x)
+
+    # Return
+    return xper
+
+
---
+++ py/desiutil/test/test_stats.py
@@ -0,0 +1,33 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""Test desiutil.setup.
+"""
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+# The line above will help with 2to3 support.
+import unittest
+import numpy as np
+import pdb
+from desiutil.stats import perc
+
+
+class TestFuncFits(unittest.TestCase):
+    """Test desiutil.funcfits
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_perc(self):
+        """Test percentile
+        """
+        x = np.linspace(0,np.pi,100)
+        y = np.sin(x)
+        percv = perc(y)
+        np.testing.assert_allclose(percv, np.array([0.22031053278654081,
+                                                   0.96384215855994215]))
