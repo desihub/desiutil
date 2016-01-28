84e8ada0de27a57c60c4ad4d5c23bde2ee9ca859
Stephen Bailey <stephen.code@gmail.com>
2016-01-28 14:42:44 -0800

    tweak docs to clarify [0,100] percentile range

--- py/desiutil/stats.py
+++ py/desiutil/stats.py
@@ -15,13 +15,12 @@ Fall 2015
 from __future__ import print_function, absolute_import, division, unicode_literals
 
 import numpy as np
-#from scipy.interpolate import interp1d
 #import pdb
 
 
 def perc(x, per=68.2):
     """ Calculate the percentile bounds of a distribution,
-    i.e. for per=0.68, the code returns the upper and lower bounds
+    i.e. for per=68, the code returns the upper and lower bounds
     that encompass 68percent of the distribution.
 
     Uses simple interpolation
@@ -31,7 +30,7 @@ def perc(x, per=68.2):
       x : float
         numpy array of values
       per : float, optional
-          Percentile for the calculation
+        Percentile for the calculation [0-100]
 
     Returns
     -------
