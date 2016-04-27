3b9688223cd46c176f07cea3f640e23b6ba2634d
J. Xavier Prochaska <xavier@ucolick.org>
2016-04-27 09:01:51 -0700

    yamlify

---
+++ py/desiutil/io.py
@@ -0,0 +1,74 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+==================
+desiutils.io
+==================
+
+Module for I/O related code
+"""
+from __future__ import (print_function, absolute_import, division,
+                        unicode_literals)
+import numpy as np
+
+
+def yamlify(obj, debug=False):
+    """ Recursively process an object so it can be serialised for yaml
+    Based on jsonify in linetools
+
+    Note: All keys in dicts are converted to str type
+
+    Parameters
+    ----------
+    obj : any object
+    debug : bool, optional
+
+    Returns
+    -------
+    obj - the same obj is yaml_friendly format
+      (arrays turned to lists, np.int64 converted to int, np.float64 to float,
+      basestring to str, etc.).
+
+    """
+    if isinstance(obj, np.float64):
+        obj = float(obj)
+    elif isinstance(obj, np.float32):
+        obj = float(obj)
+    elif isinstance(obj, np.int32):
+        obj = int(obj)
+    elif isinstance(obj, np.int64):
+        obj = int(obj)
+    elif isinstance(obj, np.int16):
+        obj = int(obj)
+    elif isinstance(obj, np.bool_):
+        obj = bool(obj)
+    elif isinstance(obj, (np.string_,basestring)):
+        obj = str(obj)
+    #elif isinstance(obj, Quantity):
+    #    obj = dict(value=obj.value, unit=obj.unit.to_string())
+    #elif isinstance(obj, np.ndarray):  # Must come after Quantity
+    #    obj = obj.tolist()
+    elif isinstance(obj, dict):
+        # First convert keys
+        obj = {str(key):value for key,value in obj.items()}
+        # Now recursive
+        for key, value in obj.items():
+            obj[key] = yamlify(value, debug=debug)
+    elif isinstance(obj, list):
+        for i,item in enumerate(obj):
+            obj[i] = yamlify(item, debug=debug)
+    elif isinstance(obj, tuple):
+        obj = list(obj)
+        for i,item in enumerate(obj):
+            obj[i] = yamlify(item, debug=debug)
+        obj = tuple(obj)
+    #elif isinstance(obj, Unit):
+    #    obj = obj.name
+    #elif obj is u.dimensionless_unscaled:
+    #    obj = 'dimensionless_unit'
+
+    if debug:
+        print(type(obj))
+    return obj
+
+
---
+++ py/desiutil/test/test_io.py
@@ -0,0 +1,41 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""Test desiutil.io.
+"""
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+# The line above will help with 2to3 support.
+import unittest
+import numpy as np
+from warnings import catch_warnings, simplefilter
+import pdb
+from ..io import yamlify
+
+
+class TestIO(unittest.TestCase):
+    """Test desiutil.io
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_yamlify(self):
+        """Test yamlify
+        """
+        fdict = {'name':'test', 'num':np.int32(3), 'flt32':np.float32(3.), 'flt64':np.float64(2.),
+                     'num2':np.int64(4), 'bool':np.bool(True), 'lst':['tst2', np.int16(2)],
+                     'tup':(1,3), 'dct':{'a':'tst3', 'b':np.float32(6.)}}
+        assert isinstance(fdict,dict)
+        assert isinstance(fdict['name'], unicode)
+        assert isinstance(fdict['flt32'], np.float32)
+        # Run
+        ydict = yamlify(fdict)
+        assert isinstance(ydict['flt32'], float)
+        for key in ydict.keys():
+            assert isinstance(key,str)
+
