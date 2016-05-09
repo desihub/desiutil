dbab85c6ebc12be76c8a47fa6c3c46813b1ab114
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2016-05-09 19:53:35 -0400

    trying to test setup commands

--- py/desiutil/test/test_setup.py
+++ py/desiutil/test/test_setup.py
@@ -6,10 +6,13 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 # The line above will help with 2to3 support.
 import os
+import sys
 import shutil
 import unittest
+from setuptools import sandbox
 from ..setup import find_version_directory, get_version, update_version
 from .. import __version__ as desiutil_version
+
 
 class TestSetup(unittest.TestCase):
     """Test desiutil.setup.
@@ -20,11 +23,64 @@ class TestSetup(unittest.TestCase):
         cls.data_dir = os.path.join(os.path.dirname(__file__), 't')
         cls.setup_dir = os.path.abspath('.')
         cls.fake_name = 'frobulate'
+        cls.original_dir = os.getcwd()
 
     @classmethod
     def tearDownClass(cls):
         shutil.rmtree(os.path.join(cls.setup_dir, cls.fake_name),
                       ignore_errors=True)
+        shutil.rmtree(os.path.join(cls.data_dir, cls.fake_name),
+                      ignore_errors=True)
+
+    def setUp(self):
+        os.chdir(self.original_dir)
+
+    def tearDown(self):
+        os.chdir(self.original_dir)
+
+    def run_setup(self, *args, **kwargs):
+        """In Python 3, on MacOS X, the import cache has to be invalidated
+        otherwise new extensions built with ``run_setup`` do not always get
+        picked up.
+        """
+        try:
+            return sandbox.run_setup(*args, **kwargs)
+        finally:
+            if sys.version_info[:2] >= (3, 3):
+                import importlib
+                importlib.invalidate_caches()
+
+    def not_ready_for_test_version(self):
+        """Test python setup.py version.
+        """
+        path_index = int(sys.path[0] == '')
+        sys.path.insert(path_index, os.path.abspath('./py'))
+        package_dir = os.path.join(self.data_dir, self.fake_name)
+        os.mkdir(package_dir)
+        os.mkdir(os.path.join(package_dir, self.fake_name))
+        os.mkdir(os.path.join(package_dir, '.git'))
+        setup = """#!/usr/bin/env python
+from setuptools import setup
+from desiutil.setup import DesiModule, DesiTest, DesiVersion, get_version
+CMDCLASS = {{'version': DesiVersion}}
+VERSION = get_version("{0.fake_name}")
+setup(name="{0.fake_name}",
+    version=VERSION,
+    packages=["{0.fake_name}"],
+    cmdclass=CMDCLASS,
+    zip_safe=False)
+""".format(self)
+        with open(os.path.join(package_dir, 'setup.py'), 'w') as s:
+            s.write(setup)
+        init = """from ._version import __version__
+"""
+        with open(os.path.join(package_dir, self.fake_name, '__init__.py'), 'w') as i:
+            i.write(init)
+        os.chdir(package_dir)
+        self.run_setup('setup.py', ['version'])
+        os.chdir(self.original_dir)
+        shutil.rmtree(package_dir)
+        del sys.path[path_index]
 
     def test_find_version_directory(self):
         """Test the search for a _version.py file.
@@ -76,7 +132,6 @@ class TestSetup(unittest.TestCase):
         os.remove(os.path.join(p, '_version.py'))
         p2 = os.path.join(self.setup_dir, self.fake_name, self.fake_name)
         os.makedirs(p2)
-        original_dir = os.getcwd()
         os.chdir(p)
         with self.assertRaises(IOError):
             update_version(self.fake_name)
@@ -90,6 +145,6 @@ class TestSetup(unittest.TestCase):
         self.assertTrue(os.path.exists(os.path.join(p2, '_version.py')))
         os.remove(os.path.join(p2, '_version.py'))
         os.rmdir(os.path.join(p, '.svn'))
-        os.chdir(original_dir)
+        os.chdir(self.original_dir)
         os.rmdir(p2)
         os.rmdir(p)
