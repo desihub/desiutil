a03efc078815035bc52ccb83420ddd673626a412
J. Xavier Prochaska <xavier@ucolick.org>
2016-07-30 06:07:59 -0700

    xlim edit

--- py/desiutil/plots.py
+++ py/desiutil/plots.py
@@ -18,7 +18,8 @@ except NameError:  # For Python 3
     basestring = str
 
 
-def plot_slices(x, y, x_lo, x_hi, y_cut, num_slices=5, min_count=100, axis=None):
+def plot_slices(x, y, x_lo, x_hi, y_cut, num_slices=5, min_count=100, axis=None,
+                set_ylim_from_stats=True):
     """Scatter plot with 68, 95 percentiles superimposed in slices.
     Modified from code written by D. Kirkby
 
@@ -49,6 +50,8 @@ def plot_slices(x, y, x_lo, x_hi, y_cut, num_slices=5, min_count=100, axis=None)
         statistics.
     axis : matplotlib axis object or None
         Uses the current axis if this is None.
+    set_ylim_from_stats : bool, optional
+        Set ylim of plot from 95% stat
     """
     import matplotlib.pyplot as plt
 
@@ -82,6 +85,7 @@ def plot_slices(x, y, x_lo, x_hi, y_cut, num_slices=5, min_count=100, axis=None)
     y_p1 = stepify(limits[:, 3])
     y_p2 = stepify(limits[:, 4])
     xstack = stepify(x_bins)[1:-1]
+    max_yr, max_p2, min_m2 = 0., 0., 0.
     for i in range(num_slices):
         s = slice(2 * i, 2 * i + 2)
         if counts[i] >= min_count:
@@ -90,6 +94,17 @@ def plot_slices(x, y, x_lo, x_hi, y_cut, num_slices=5, min_count=100, axis=None)
             axis.fill_between(
                 xstack[s], y_m1[s], y_p1[s], alpha=0.25, color='red')
             axis.plot(xstack[s], y_med[s], 'r-', lw=2.)
+            # For ylim
+            max_yr = max(max_yr, np.max(y_p2[s]-y_m2[s]))
+            max_p2 = max(max_p2, np.max(y_p2[s]))
+            min_m2 = min(min_m2, np.min(y_m2[s]))
+
+    # xlim
+    axis.set_xlim(np.min(x), np.max(x))
+
+    # ylim
+    if set_ylim_from_stats:
+        axis.set_ylim(min_m2-max_yr, max_p2+max_yr)
 
     # Plot cut lines.
     axis.axhline(+y_cut, ls=':', color='k')
