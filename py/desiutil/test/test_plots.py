a787954070b3a0aacc79714103dad057a4666d8d
fjaviersanchez <francs1@uci.edu>
2016-11-28 17:59:34 -0800

    Changed test_plots to include tempdir

--- py/desiutil/test/test_plots.py
+++ py/desiutil/test/test_plots.py
@@ -6,6 +6,7 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 # The line above will help with 2to3 support.
 import unittest
+import shutil, tempfile
 import os
 import numpy as np
 import sys
@@ -25,13 +26,13 @@ class TestPlotSlices(unittest.TestCase):
     """
 
     @classmethod
-    def setUpClass(cls):
-        cls.plot_file = 'test.png'
+    def setUpClass(self):
+        self.test_dir = tempfile.mkdtemp()
+        self.plot_file = os.path.join(self.test_dir,'test_slices.png')
    
     @classmethod
-    def tearDownClass(cls):
-        if os.path.exists(cls.plot_file):
-            os.remove(cls.plot_file)
+    def tearDownClass(self):
+        shutil.rmtree(self.test_dir)       
  
     def test_slices(self):
         """Test plot_slices
@@ -41,22 +42,21 @@ class TestPlotSlices(unittest.TestCase):
         x = np.random.rand(1000)
         y = np.random.randn(1000)
         # Run 
-        ax_slices = plot_slices(x,y,0.,1.,0.)
-        print('Created plot_slices')
+        ax_slices = plot_slices(x,y,0.,1.,0.) 
         ax_slices.set_ylabel('N sigma')
         ax_slices.set_xlabel('x')
-        print('Changed labels')
-        plt.savefig(self.plot_file)
+        fig = plt.gcf()
+        fig.savefig(self.plot_file)
         print('Saved file')
 class TestPlotSky(unittest.TestCase):    
     @classmethod
-    def setUpClass(cls):
-        cls.plot_file2 = 'test_sky.png'
-
+    def setUpClass(self):
+        self.test_dir = tempfile.mkdtemp()
+        self.plot_file = os.path.join(self.test_dir,'test_sky.png')
+   
     @classmethod
-    def tearDownClass(cls):
-        if os.path.exists(cls.plot_file2):
-            os.remove(cls.plot_file2)
+    def tearDownClass(self):
+        shutil.rmtree(self.test_dir)    
 
     def test_plot_sky(self):
         """Test plot_sky
@@ -66,7 +66,7 @@ class TestPlotSky(unittest.TestCase):
         dec = 360.*np.random.rand(200)
         #Run
         ax = plot_sky(ra,dec,discrete_colors=False,pix_shape='square')
-        plt.savefig(self.plot_file2)
+        plt.savefig(self.plot_file)
 
 def test_suite():
     """Allows testing of only this module with the command::
