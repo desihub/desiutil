a091b039d8a7100fba3edaa5f16d2ed6e030612d
Benjamin Alan Weaver <baweaver@lbl.gov>
2017-01-06 16:46:15 -0700

    fix documentation build error

--- doc/conf.py
+++ doc/conf.py
@@ -131,7 +131,7 @@ napoleon_include_private_with_doc = True
 # This value contains a list of modules to be mocked up. This is useful when
 # some external dependencies are not met at build time and break the
 # building process.
-autodoc_mock_imports = ['numpy']
+autodoc_mock_imports = ['numpy', 'numpy.ma']
 
 # -- Options for HTML output ----------------------------------------------
 
