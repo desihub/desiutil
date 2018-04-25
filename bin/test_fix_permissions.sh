a4232b3e02c25f436c8179fa3c795f01cb8d1ee2
Benjamin Alan Weaver <baweaver@lbl.gov>
2018-04-24 17:24:16 -0700

    use getfacl instead of stat [skip ci]

--- bin/test_fix_permissions.sh
+++ bin/test_fix_permissions.sh
@@ -6,8 +6,8 @@
 #
 dirPerm=(0777 0775 0750 0700)
 filePerm=(0666 0664 0640 0600)
-fixedDirPerm=(2770 2770 2750 2750)
-fixedFilePerm=(660 660 640 640)
+fixedDirPerm=(group::rwx group::rwx group::r-x group::r-x)
+fixedFilePerm=(group::rw- group::rw- group::r-- group::r--)
 for k in $(seq 0 3); do
     echo /bin/rm -rf Dir${k}
     /bin/rm -rf Dir${k}
@@ -21,10 +21,10 @@ for k in $(seq 0 3); do
     chmod ${filePerm[$k]} Dir${k}/File${k}
     echo fix_permissions.sh -v Dir${k}
     fix_permissions.sh -v Dir${k}
-    [[ $(stat -c %a Dir${k}) == ${fixedDirPerm[$k]} ]] || echo "Dir${k}/ permission not set properly!"
+    [[ $(getfacl -c Dir${k} | grep group) == ${fixedDirPerm[$k]} ]] || echo "Dir${k}/ permission not set properly!"
     [[ $(stat -c %G Dir${k}) == desi ]] || echo "Dir${k}/ group ID not set properly!"
     [[ $(getfacl -c Dir${k} | grep desi) == user:desi:rwx ]] || echo "Dir${k}/ ACL not set properly!"
-    [[ $(stat -c %a Dir${k}/File${k}) == ${fixedFilePerm[$k]} ]] || echo "Dir${k}/File${k} permission not set properly!"
+    [[ $(getfacl -c Dir${k}/File${k} | grep group) == ${fixedFilePerm[$k]} ]] || echo "Dir${k}/File${k} permission not set properly!"
     [[ $(stat -c %G Dir${k}/File${k}) == desi ]] || echo "Dir${k}/File${k} group ID not set properly!"
     [[ $(getfacl -c Dir${k}/File${k} | grep desi) == user:desi:rw- ]] || echo "Dir${k}/File${k} ACL not set properly!"
 done
