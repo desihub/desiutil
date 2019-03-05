3ccea866c558d718412bd567425def47fdaefcd1
Benjamin Alan Weaver <baweaver@lbl.gov>
2019-03-05 09:31:47 -0700

    fix file path

--- bin/user_metadata.sh
+++ bin/user_metadata.sh
@@ -4,8 +4,9 @@ uid=$(id -u ${user})
 date=$2
 group=desi
 gid=58102
+root=/global/project/projectdirs/${group}/metadata
 for f in 2 a; do
-    grep -E "${uid}\|${desi}" ${date}.tlproject${f}.${group}.txt | \
+    grep -E "${uid}\|${desi}" ${root}/${date}.tlproject${f}.${group}.txt | \
         cut -d\| -f16 | \
         sed 's$%2F$/$g' | \
         sed "s%.snapshots/${date}/%%g"
