3cac6ba9a4d2cecfd2c43b4bab7717cec2e0ce54
Benjamin Alan Weaver <baweaver@lbl.gov>
2020-04-08 11:26:36 -0700

    don't use explicit url

--- py/desiutil/plots.py
+++ py/desiutil/plots.py
@@ -1064,7 +1064,7 @@ def plot_iers(which='auto', num_points=500, save=None):
         iers = iers.IERS_B.open()
     elif which == 'A':
         # This requires network access to download the latest file.
-        iers = iers.IERS_A.open(iers.IERS_A_URL)
+        iers = iers.IERS_A.open()
     elif which == 'frozen':
         freeze_iers()
         iers = iers.IERS_Auto.open()
