79f928ed0e4dcec6a074c09b12db093ee4541d72
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2025-04-23 09:50:46 -0700

    working on out test

--- py/desiutil/test/test_bitmask.py
+++ py/desiutil/test/test_bitmask.py
@@ -197,5 +197,7 @@ class TestBitMask(unittest.TestCase):
             new_array |= self.ccdmask.HOT
             self.assertTrue((new_array == (array + 18)).all())
             self.assertTrue((np.bitwise_and(new_array, self.ccdmask.HOT) != 0).all())
-            out_array=array.copy()
-            self.assertTrue((np.bitwise_xor(new_array, self.ccdmask.BAD, out=out_array) == array).all())
+            out_array = array.copy()
+            # self.assertTrue((np.bitwise_xor(new_array, self.ccdmask.BAD, out=out_array) == array).all())
+            np.bitwise_xor(new_array, self.ccdmask.BAD, out=out_array)
+            self.assertTrue((out_array == array).all())
